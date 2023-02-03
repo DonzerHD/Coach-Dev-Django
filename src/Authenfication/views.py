a7f5b75e1da02db21ad73595cc7e0a9ba86f6b4d
DonzerHD <thomas.lemay59@outlook.fr>
2023-02-03 23:41:57 +0100

    modification login

--- src/Authenfication/views.py
+++ src/Authenfication/views.py
@@ -1,26 +1,2 @@
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
-
-from Authenfication.forms import ConnexionForm
-from Authenfication.models import Utilisateur
 
 
-def connexion(request):
-    if request.method == "POST":
-            form = ConnexionForm(request.POST)
-            if form.is_valid():
-                email = form.cleaned_data["email"]
-                password = form.cleaned_data["password"]
-                utilisateur = Utilisateur.objects.filter(email=email)
-                if utilisateur:
-                    utilisateur = utilisateur[0]
-                    if utilisateur.password == password:
-                        request.session['utilisateur_id'] = utilisateur.id
-                        return redirect('accueil')
-                    else:
-                        form.add_error(None, "Email ou mot de passe incorrect")
-                else:
-                    form.add_error(None, "Email ou mot de passe incorrect")
-    else:
-            form = ConnexionForm()
-    return render(request, 'Authenfication/login.html', locals())
--- src/Coach_Dev_Project/urls.py
+++ src/Coach_Dev_Project/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from Authenfication.views import connexion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.accueil, name='accueil' ),
-    path("login/", connexion, name="connexion"),
 ]
